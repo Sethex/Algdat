22ee43f90ac7525241ec9100cdac368c788d194f
Sivert Underdal <sivertun@stud.ntnu.no>
2024-10-04 11:08:26 +0200

    add other ovinger

---
+++ Oving2/OvingforelesnKo.py
@@ -0,0 +1,22 @@
+class Queue:
+    def __init__():
+        pass
+
+    def enqueue():
+        pass
+
+    def dequeue():
+        pass
+
+
+myQueue = Queue()
+
+switched = True
+while switched == True:
+    switched = False
+    ## Dequeue første og andre element, compare de, og enqueue det laveste elementet.
+    ## Dequeue neste element, compare med forrige element, og enqueue det laveste elementet.
+    ## Hvis noen av elementene bytte plass, sett switched til True
+    ## Gjør dette til du er kommet rundt køen, og sjekk om det har blitt switcha noe.
+    ## 
+    
